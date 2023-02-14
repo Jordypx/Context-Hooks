3c3e9de01d009208b0c7e4f3ec0b217d169b96db
jordypx <jordanodo89@gmail.com>
2023-02-14 13:01:38 -0700

    Todays Commit

---
+++ src/components/NavBar.js
@@ -0,0 +1,15 @@
+
+const NavBar = () => {
+    return (
+        <nav>
+            <h1>Context App</h1>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+    );
+}
+
+export default NavBar;
