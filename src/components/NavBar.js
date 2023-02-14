d6e16872511f3573d237fb4183662d11226fbc32
jordypx <jordanodo89@gmail.com>
2023-02-14 14:19:11 -0700

    new

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,14 @@
+import React, { useContext } from 'react';
+import { ThemeContext } from "../context/ThemeContext";
+
 
 const NavBar = () => {
+    const { isLightTheme, light, dark } = useContext(ThemeContext);
+
+    const theme = isLightTheme ? light : dark;
+
     return (
-        <nav>
+        <nav style={{ color: theme.syntax, background: theme.ui }}>
             <h1>Context App</h1>
             <ul>
                 <li>Home</li>
